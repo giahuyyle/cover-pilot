fa62e35d3c50136d2245c696088fd25ed45fb8df
Gia Huy Le <huyyyylegia@gmail.com>
2026-03-01 19:40:42 -0700

    chore: init pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router";
 import RootLayout from "./layouts/RootLayout";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <Routes>
       <Route element={<RootLayout />}>
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
   );
--- frontend/src/layouts/RootLayout.jsx
+++ frontend/src/layouts/RootLayout.jsx
@@ -3,6 +3,12 @@ import { Outlet } from "react-router";
 export default function RootLayout() {
   return (
     <div className="min-h-screen flex flex-col">
+
+      {/* Outlet is to render the children element */}
+      <main className="">
+        <Outlet />
+      </main>
+
     </div>
   );
 }
